8d981df54f9d7c4b3fc2558b59b09168bd3684cb
Prem kumar <premhbhai@gmail.com>
2026-09-13 17:43:33 +0530

    Fix GitHub Pages setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: '/Portfolio-main/',
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio-main/',
-})
+  base: "/Portfolio-main/",
+});
